af9b7596a090b937a4104c8beacc303b799c5aa0
Andrej Gajdos <mail@andrejgajdos.com>
2020-05-30 14:52:14 +0200

    init commit
    init commit

---
+++ routes/send-email.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var router = express.Router();
+var nodemailer = require("nodemailer");
+require("dotenv").config({
+  path: `.env.keys`,
+});
+
+const getHostname = (url) => {
+  // use URL constructor and return hostname
+  return new URL(url).hostname;
+};
+
+router.post("/", function (req, res, next) {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.SOURCE_ADDRESS,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.SOURCE_ADDRESS,
+    to: process.env.DESTINATION_ADDRESS,
+    subject: "[survey-widget] " + getHostname(req.body.url), // Subject line
+    text: JSON.stringify(req.body, null, 4),
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(400).send({
+        message: error,
+      });
+    }
+    res.send("Message send Successfully");
+  });
+});
+
+module.exports = router;
